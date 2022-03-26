991d85c071f90731c99ce2a17dfbf952bc097b22
VeraKupriyanova <veruna13@mail.ru>
2022-03-26 18:17:09 +0300

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,68 @@
+import java.util.List;
+import java.util.ArrayList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.*;
+import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+public class Main {
+    public static void main(String[] args)  throws ParserConfigurationException, IOException , SAXException {
+
+        //Задача 2: XML - JSON парсер
+        String xmlFileName = "data.xml";
+        List<Employee> list2 = parseXML(xmlFileName);
+        String json2 = listToJson(list2);
+        String jsonFilename2 = "data2.json";
+        writeString(json2, jsonFilename2);
+    }
+
+    private static List<Employee> parseXML(String xmlFilename) throws ParserConfigurationException, IOException , SAXException {
+        List<String> elements = new ArrayList<>();
+        List<Employee> list = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(xmlFilename));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeName().equals("employee")) {
+                NodeList nodeList1 = node.getChildNodes();
+                for (int j = 0; j < nodeList1.getLength(); j++) {
+                    Node node_ = nodeList1.item(j);
+                    if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                        elements.add(node_.getTextContent());
+                    }
+                }
+                list.add(new Employee(
+                        Long.parseLong(elements.get(0)),
+                        elements.get(1),
+                        elements.get(2),
+                        elements.get(3),
+                        Integer.parseInt(elements.get(4))));
+                elements.clear();
+            }
+        }
+        return list;
+    }
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        return gson.toJson(list, listType);
+    }
+    private static void writeString(String json, String jsonFilename) {
+        try (FileWriter file = new FileWriter(jsonFilename)) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
